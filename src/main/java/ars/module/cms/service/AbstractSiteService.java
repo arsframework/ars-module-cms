c17869a3610ec8f7e4dbc94c5ae352b6365e332b
wuyq <314691309@qq.com>
2018-03-15 16:11:08 +0800

    Add get template list api

--- src/main/java/ars/module/cms/service/AbstractSiteService.java
+++ src/main/java/ars/module/cms/service/AbstractSiteService.java
@@ -1,7 +1,13 @@
 package ars.module.cms.service;
 
+import java.util.List;
+
+import ars.util.Strings;
+import ars.file.Describe;
+import ars.file.disk.DiskOperator;
 import ars.module.cms.model.Site;
 import ars.module.cms.service.SiteService;
+import ars.invoke.request.Requester;
 import ars.database.service.StandardGeneralService;
 
 /**
@@ -13,5 +19,19 @@ import ars.database.service.StandardGeneralService;
  *            数据模型
  */
 public abstract class AbstractSiteService<T extends Site> extends StandardGeneralService<T> implements SiteService<T> {
+	private String templateDirectory = Strings.TEMP_PATH; // 模板文件目录
+
+	public String getTemplateDirectory() {
+		return templateDirectory;
+	}
+
+	public void setTemplateDirectory(String templateDirectory) {
+		this.templateDirectory = Strings.getRealPath(templateDirectory);
+	}
+
+	@Override
+	public List<Describe> templates(Requester requester) throws Exception {
+		return new DiskOperator(this.templateDirectory).trees(null, requester.getParameters());
+	}
 
 }
--- src/main/java/ars/module/cms/service/SiteService.java
+++ src/main/java/ars/module/cms/service/SiteService.java
@@ -1,8 +1,12 @@
 package ars.module.cms.service;
 
+import java.util.List;
+
+import ars.file.Describe;
 import ars.invoke.local.Api;
-import ars.module.cms.model.Site;
+import ars.invoke.request.Requester;
 import ars.database.service.BasicService;
+import ars.module.cms.model.Site;
 
 /**
  * 站点业务操作接口
@@ -14,5 +18,16 @@ import ars.database.service.BasicService;
  */
 @Api("cms/site")
 public interface SiteService<T extends Site> extends BasicService<T> {
+	/**
+	 * 获取站点模板
+	 * 
+	 * @param requester
+	 *            请求对象
+	 * @return 文件描述对象树列表
+	 * @throws Exception
+	 *             操作异常
+	 */
+	@Api("templates")
+	public List<Describe> templates(Requester requester) throws Exception;
 
 }
