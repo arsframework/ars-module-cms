7d7717186a2e034f2015d1b115787356934ef9f1
arsframework <postmaster@arsframework.com>
2018-03-26 18:29:22 +0800

    Update parameters

--- src/main/java/ars/module/cms/tags/channel/ListingTag.java
+++ src/main/java/ars/module/cms/tags/channel/ListingTag.java
@@ -6,6 +6,7 @@ import java.util.List;
 import ars.module.cms.tags.Paging;
 import ars.module.cms.tags.Listing;
 import ars.module.cms.tags.AbstractCmsTag;
+import ars.database.repository.Query;
 import ars.invoke.channel.http.HttpRequester;
 
 /**
@@ -20,8 +21,16 @@ public class ListingTag extends AbstractCmsTag {
 	protected Object execute() throws Exception {
 		HttpRequester requester = this.getRequester();
 		Map<String, Object> parameters = this.getParameters();
+		Integer page = (Integer) parameters.remove(Query.PAGE);
+		Integer size = (Integer) parameters.remove(Query.SIZE);
 		Integer count = (Integer) requester.execute("cms/channel/count", parameters);
 		Paging paging = new Paging(count, this.getPage(), this.getSize());
+		if (page != null) {
+			parameters.put(Query.PAGE, page);
+		}
+		if (size != null) {
+			parameters.put(Query.SIZE, size);
+		}
 		List<?> objects = (List<?>) requester.execute("cms/channel/objects", parameters);
 		return new Listing(paging, objects);
 	}
--- src/main/java/ars/module/cms/tags/content/ListingTag.java
+++ src/main/java/ars/module/cms/tags/content/ListingTag.java
@@ -6,6 +6,7 @@ import java.util.List;
 import ars.module.cms.tags.Paging;
 import ars.module.cms.tags.Listing;
 import ars.module.cms.tags.AbstractCmsTag;
+import ars.database.repository.Query;
 import ars.invoke.channel.http.HttpRequester;
 
 /**
@@ -20,11 +21,19 @@ public class ListingTag extends AbstractCmsTag {
 	protected Object execute() throws Exception {
 		HttpRequester requester = this.getRequester();
 		Map<String, Object> parameters = this.getParameters();
+		Integer page = (Integer) parameters.remove(Query.PAGE);
+		Integer size = (Integer) parameters.remove(Query.SIZE);
 		if (parameters.isEmpty()) {
 			return new Listing(new Paging(0, this.getPage(), this.getSize()));
 		}
 		Integer count = (Integer) requester.execute("cms/content/count", parameters);
 		Paging paging = new Paging(count, this.getPage(), this.getSize());
+		if (page != null) {
+			parameters.put(Query.PAGE, page);
+		}
+		if (size != null) {
+			parameters.put(Query.SIZE, size);
+		}
 		List<?> objects = (List<?>) requester.execute("cms/content/objects", parameters);
 		return new Listing(paging, objects);
 	}
